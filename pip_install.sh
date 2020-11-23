49cd891548f3d11571a2265bf945c8ad0f0b8385
mervyn <mervyn_wang@jswpac.com>
2020-11-23 17:28:21 +0800

    update

--- pip_install.sh
+++ pip_install.sh
@@ -7,8 +7,11 @@
 # pip3 install nltk wordcloud matplotlib
 
 # for run.py
-pip3 install gensim numpy importlib argparse tqdm datetime pandas sklearn tensorboardX
+pip3 install gensim numpy importlib argparse tqdm datetime pandas sklearn tensorboardX nltk
+
+python -c "import nltk; nltk.download('punkt')"
 
 # no gpu
 pip3 install torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+
 
